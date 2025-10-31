d634fae55f670066cb6a09af0243c8996d9aed83
marley <116856059+Marleyedrg@users.noreply.github.com>
2025-10-31 16:31:58 -0300

    remake circular DoublyList🥵

--- src/DoublyList.ts
+++ src/DoublyList.ts
@@ -1,111 +1,104 @@
-import { throwDeprecation } from "process";
-
 interface Node<T> {
   prev: Node<T> | null,
   data: T,
   next: Node<T> | null
 }
-
 export default class DoublyList<T> {
-  public head: Node<T> | null = null;
-  public tail: Node<T> | null = this.head;
+
+  head: Node<T> | null = null;
+  tail: Node<T> | null = null;
 
   protected circular() {
 
-    if (this.head === null || this.tail === null) {
-      throw new Error;
+    if (!this.head || !this.tail) {
+      throw Error("head and tail don't exist here!!!")
     }
 
     this.head.prev = this.tail;
-
     this.tail.next = this.head;
   }
 
-  public add(value: T) {
-
+  add(value: T) {
     const newNode: Node<T> = {
       prev: null,
       data: value,
       next: null
-    }
+    };
 
     if (this.head === null) {
       this.head = newNode;
-
       this.tail = newNode;
-
       this.circular();
       return;
     }
 
-    if (this.tail === null) {
-      throw new Error;
-    }
+    if (!this.head || !this.tail) {
+      throw Error("head and tail don't exist here!!!")
+    }//type safety
 
-    const lastTail: Node<T> = this.tail;
+    let lastTail: Node<T> = this.tail;
+    //save the current tail to last tail, to switch with new node in future
+
+    lastTail.next = newNode;
+    //The last tail next need to pointer to new tail 
 
     this.tail = newNode;
-
+    //new tail need to be the new node
     this.tail.prev = lastTail;
-
-    lastTail.next = this.tail;
+    //the new tail prev need to pointer to last tail
 
     this.circular();
+    //make circular again
   }
 
-  public remove(value: T) {
-    if (!this.head || !this.tail) {
-      throw new Error("Nothing to remove!!!")
-    }
+  remove(value: T) {
+
+    if (!this.head || !this.tail || !this.head.next) {
+      throw Error("head and tail don't exist here!!!")
+    }//type safety
 
     if (this.head.data === value) {
-
-      if (this.head === this.tail) {
-        this.head = null;
-        this.tail = null;
-        return;
-      }
-
       this.head = this.head.next;
       this.circular();
       return;
     }
+    //case where value is on head
 
     if (this.tail.data === value) {
       this.tail = this.tail.prev;
       this.circular();
       return;
     }
-
-    if (!this.head.next) {//type guard
-      throw new Error("even though it's impossible for head.next to be null, typescript thinks it can be, because I initialized these properties as null, and allowed for the possibility of them being null")
-    }
+    //case where value is on tail
 
     let currNode: Node<T> = this.head.next;
 
-    while (currNode !== this.head && currNode.data !== value) {
-      if (currNode.next === null) {
-        throw new Error;
+    //currNode.next -> type safety
+    while (currNode.next && currNode.data !== value) {
+
+      //If currNode is equal to head, it means we have made a full circle.
+      if (currNode == this.head) {
+        throw new Error(`value (${value}) not find in ${DoublyList} to remove !!!`)
       }
+      //go to next Node, nothing that we need here!
       currNode = currNode.next;
     }
 
-    if (currNode.data !== value) {
-      // The loop finished by returning to the head, so the value was not found.
-      throw new Error(`${value} was not found in the List!!!`)
+    //currNode.next && currNode.prev -> type safety
+    if ((currNode.next && currNode.prev) && currNode.data === value) {
+      //(a <-> currNode <-> c)
+
+      //lets remove currNode
+
+      const a: Node<T> = currNode.prev;
+      const c: Node<T> = currNode.next;
+
+      a.next = c;
+      //a.next = c
+
+      c.prev = a;
+      //c.prev = a
       return;
     }
-
-    if (currNode.prev) {//type guard
-      currNode.prev.next = currNode.next;
-    }
-
-    if (currNode.next) {//type guard
-      currNode.next.prev = currNode.prev;
-    }
   }
-
-
-
-
 }
